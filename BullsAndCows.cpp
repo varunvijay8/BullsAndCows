ccafba0581c917138590302d338a95eb61dea1e8
varunvijay8 <varunvijay8@gmail.com>
2016-12-26 23:27:45 -0800

    Update BullsAndCows.cpp

--- BullsAndCows.cpp
+++ BullsAndCows.cpp
@@ -4,23 +4,40 @@
 using namespace std;
 
 void PrintIntro();
-string GetGuessAndPrint();
+void PlayGame(const int &NumberOfGuesses);
+string GetGuess();
+void PrintGuess(string Guess);
+bool AskToPlayAgain();
 
 int main()
 {
 
 	constexpr int NumberOfGuesses = 5;
-	string Guess = "";
-
+	bool ContinuePlaying = false;
+	
 	PrintIntro();
 
-	for (int i = 0; i < NumberOfGuesses; i++)
-	{
-		Guess = GetGuessAndPrint();
-	}
+	PlayGame(NumberOfGuesses);
+
+	ContinuePlaying = AskToPlayAgain();
+
+	cout << "So we are going to continue? " << ContinuePlaying << endl;
+	cout << endl;
 
 	return 0;
 }
+
+void PlayGame(const int &NumberOfGuesses)
+{
+	string Guess = "";
+
+	for (int i = 0; i < NumberOfGuesses; i++)
+	{
+		Guess = GetGuess();
+		PrintGuess(Guess);
+	}
+}
+
 
 void PrintIntro()
 {
@@ -35,17 +52,31 @@ void PrintIntro()
 	return;
 }
 
-string GetGuessAndPrint()
+string GetGuess()
 {
 	string Guess = "";
 
 	// Input guess
 	cout << "Enter your guess: ";
-	getline(cin, Guess);
+	getline(cin, Guess);	
 
+	return Guess;
+}
+
+void PrintGuess(string Guess)
+{
 	cout << "You guessed " << Guess << endl;
 
 	cout << endl;
+}
 
-	return Guess;
+bool AskToPlayAgain()
+{
+	string Response = "";
+	cout << "Do you want to play again(y/n): ";
+	getline(cin, Response);
+
+	cout << endl;
+
+	return (tolower(Response[0]) == 'y' ? true : false);
 }
