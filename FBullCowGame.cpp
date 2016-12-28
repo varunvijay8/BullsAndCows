edbcf868b13ca04fee1567f28a5fb78fbf0a8750
varunvijay8 <varunvijay8@gmail.com>
2016-12-27 17:01:07 -0800

    Create FBullCowGame.cpp

---
+++ FBullCowGame.cpp
@@ -0,0 +1,25 @@
+#include "FBullCowGame.h"
+
+int FBullCowGame::GetCurrentTry()
+{
+	return 0;
+}
+
+int FBullCowGame::GetMaxTries()
+{
+	return 0;
+}
+
+bool FBullCowGame::IsGameWon()
+{
+	return false;
+}
+
+bool FBullCowGame::CheckGuessValidity(std::string)
+{
+	return false;
+}
+
+void FBullCowGame::Reset()
+{
+}
