75b790671408e39401d09f891ff7e4b14f436dbc
varunvijay8 <varunvijay8@gmail.com>
2017-04-01 09:06:11 -0700

    Update FBullCowGame.h

--- FBullCowGame.h
+++ FBullCowGame.h
@@ -1,14 +1,32 @@
 #pragma once
 
 #include <string>
+#include <map>
+
+#define TMap std::map
 
 using FString = std::string;
 using int32 = int;
 
-struct BullCowCount
+struct FBullCowCount
 {
 	int32 Bulls = 0;
 	int32 Cows = 0;
+};
+
+enum class EGuessStatus
+{
+	Invalid,
+	OK,
+	Not_Isogram,
+	Wrong_Length,
+	Not_LowerCase
+};
+
+enum class EResetStatus
+{
+	OK,
+	No_Hidden_Word
 };
 
 class FBullCowGame {
@@ -17,14 +35,20 @@ public:
 
 	int32 GetCurrentTry() const;
 	int32 GetMaxTries() const;
+	int32 GetHiddenWordLength() const;
 	bool IsGameWon() const;
+	
+	EGuessStatus CheckGuessValidity(FString) const;
 
-	bool CheckGuessValidity(FString);
 	void Reset(const int32 &DefaultCurrentTry, const int32 &MaxTries);	
-	BullCowCount SubmitGuess(FString);
-
+	FBullCowCount SubmitValidGuess(FString);
+	
 private:
 	int32 MyCurrentTry;
 	int32 MyMaxTries;
 	FString MyHiddenWord;
+	bool bGameWon;
+	
+	bool IsIsogram(FString) const;
+	bool IsLowerCase(FString) const;
 };
